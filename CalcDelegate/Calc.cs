caa4227be56f89e2b19ec1b0951c1507c1b0f0f0
Vitaly <mickelangello45@gmail.com>
2018-12-15 22:07:56 +0200

    adds

---
+++ CalcDelegate/Calc.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+
+
+namespace CalcDelegate
+{
+    class Calc
+    {
+        public delegate void LogToConsole(string msg);
+        LogToConsole _l;
+        public Settings s = File.Exists("json.json") ? JsonConvert.DeserializeObject<Settings>(File.ReadAllText("json.json")) : new Settings { };
+
+        public Calc(LogToConsole l)
+        {
+            _l = l;
+        }
+
+        
+        public void Sum()
+        {
+            int result = 0;
+            string temp = " ";
+            for (int i = 0; i < s.Operations.Length; i++)
+            {
+                if (s.Operations[i] == '+')
+                {
+                    for (int j = 0; j < s.Numbers.Length; j++)
+                    {
+                        if (s.Numbers[j] == ',')
+                            continue;
+                        else
+                        {
+                            if (j + 1 < s.Numbers.Length)
+                            {
+                                if (s.Numbers[j + 1] != ',')
+                                {
+                                    temp += s.Numbers[j].ToString();
+                                    continue;
+                                }
+                            }
+                            temp += s.Numbers[j].ToString();
+
+                            result += int.Parse(temp.ToString());
+
+                            temp = " ";
+                        }
+                    }
+                    break;
+                }
+            }
+            _l($"Sum of numbers {s.Numbers}: {result}");
+        }
+
+        
+        public void Sub()
+        {
+            int result = 0;
+            string temp = "";
+            for (int i = 0; i < s.Operations.Length; i++)
+            {
+                if (s.Operations[i] == '-')
+                {
+                    for (int j = 0; j < s.Numbers.Length; j++)
+                    {
+                        if (s.Numbers[j] == ',')
+                            continue;
+                        else
+                        {
+                            if (j + 1 < s.Numbers.Length)
+                            {
+                                if (s.Numbers[j + 1] != ',')
+                                {
+                                    temp += s.Numbers[j].ToString();
+                                    continue;
+                                }
+                            }
+                            temp += s.Numbers[j].ToString();
+
+                            if (j == 0)
+                                result = int.Parse(s.Numbers[j].ToString());
+                            else
+                                result -= int.Parse(temp.ToString());
+
+                            temp = "";
+                        }
+                    }
+                    break;
+                }
+            }
+            _l($"Sub of numbers {s.Numbers}: {result}");
+        }
+
+        public void Multiplication()
+        {
+            int result = 0;
+            string temp = "";
+            for (int i = 0; i < s.Operations.Length; i++)
+            {
+                if (s.Operations[i] == '*')
+                {
+                    for (int j = 0; j < s.Numbers.Length; j++)
+                    {
+                        if (s.Numbers[j] == ',')
+                            continue;
+                        else
+                        {
+                            if (j + 1 < s.Numbers.Length)
+                            {
+                                if (s.Numbers[j + 1] != ',')
+                                {
+                                    temp += s.Numbers[j].ToString();
+                                    continue;
+                                }
+                            }
+                            temp += s.Numbers[j].ToString();
+
+                            if (j == 0)
+                                result = int.Parse(s.Numbers[j].ToString());
+                            else if (result == 0 && temp != "")
+                                result = int.Parse(temp.ToString());
+                            else
+                                result *= int.Parse(temp.ToString());
+
+                            temp = "";
+                        }
+                    }
+                    break;
+                }
+            }
+            _l($"Multiplication of numbers {s.Numbers}: {result}");
+        }
+
+        public void Dividing()
+        {
+            double result = 0;
+            string temp = "";
+            for (int i = 0; i < s.Operations.Length; i++)
+            {
+                if (s.Operations[i] == '/')
+                {
+                    for (int j = 0; j < s.Numbers.Length; j++)
+                    {
+                        if (s.Numbers[j] == ',')
+                            continue;
+                        else
+                        {
+                            if (j + 1 < s.Numbers.Length)
+                            {
+                                if (s.Numbers[j + 1] != ',')
+                                {
+                                    temp += s.Numbers[j].ToString();
+                                    continue;
+                                }
+                            }
+                            temp += s.Numbers[j].ToString();
+
+                            if (j == 0)
+                                result = double.Parse(s.Numbers[j].ToString());
+                            else if(result == 0 && temp != "")
+                                result = double.Parse(temp.ToString());
+                            else
+                                result /= double.Parse(temp.ToString());
+
+                            temp = "";
+                        }
+                    }
+                    break;
+                }
+            }
+            _l($"Dividing of numbers {s.Numbers}: {result}");
+        }
+    }
+}
--- CalcDelegate/Program.cs
+++ CalcDelegate/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CalcDelegate
 {
@@ -10,6 +6,17 @@ namespace CalcDelegate
     {
         static void Main(string[] args)
         {
+            Calc calc = new Calc(new Calc.LogToConsole(Show_Message));
+            calc.Sum();
+            calc.Sub();
+            calc.Multiplication();
+            calc.Dividing();
         }
+
+        private static void Show_Message(string message)
+        {
+            Console.WriteLine(message);
+        }
+
     }
 }
